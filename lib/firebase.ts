5ad5817f11770157445043f6b5bead643a13128c
DuncanBH <2031593@champlaincollege.qc.ca>
2022-12-09 15:00:09 -0500

    Clean up + Firebase config

---
+++ lib/firebase.ts
@@ -0,0 +1,16 @@
+import firebaseConfig from "./firebaseConfig";
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Initialize firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Firestore exports
+export const firestore = getFirestore(firebaseApp);
+
+//Auth Exports
+export const auth = getAuth(firebaseApp);
+export const googleProvider = new GoogleAuthProvider();
+
+export default firebaseApp;
---
+++ lib/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBjBJS4gi3dtV-MwMSHupl7gU777ctfBng",
+    authDomain: "okkoro-de23f.firebaseapp.com",
+    projectId: "okkoro-de23f",
+    storageBucket: "okkoro-de23f.appspot.com",
+    messagingSenderId: "791497622354",
+    appId: "1:791497622354:web:15bad4781b785b0ac1f08b",
+    measurementId: "G-TLB155DXWY"
+};
