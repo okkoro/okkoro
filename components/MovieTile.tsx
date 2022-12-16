be95a2e1981935509e9af80f896314f63409acb7
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2022-12-16 11:25:43 -0500

    create file and test image

---
+++ components/MovieTile.tsx
@@ -0,0 +1,7 @@
+export default function MovieTile(){
+    return (
+        <div className="card">
+            <Image src={"https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_FMjpg_UX1000_.jpg"} alt="image of movie" width={"350px"} height={"300px"}/>
+        </div>
+    )
+}
