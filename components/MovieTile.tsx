d8555daf1a2ba1e9aaef0f6a7c5576a64f4a103a
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2022-12-19 11:41:26 -0500

    improve movie tile

--- components/MovieTile.tsx
+++ components/MovieTile.tsx
@@ -1,13 +1,19 @@
 import {Card, CardGroup, CardImg} from "react-bootstrap";
 export default function MovieTile(props){
+
     return (
         <div>
-            <Card style={{width:"200px",height:"350px"}}>
+            <Card className="bg-light-gray"style={{width:"12rem"}}>
                 <CardImg src={"https://image.tmdb.org/t/p/w500" + props.movie.poster_path} alt="image of movie" />
-                <CardGroup>
-                    <h5 className="card-title">{props.movie.title}</h5>
-                    <p className="card-text">{props.movie.vote_average}</p>
-                </CardGroup>
+                <Card.Body>
+                    <Card.Title>{props.movie.title}</Card.Title>
+                    <Card.Text className={"text-start"}>
+                        <b>{props.movie.release_date.substring(0,4)}</b>
+                        <br/>
+                        {props.movie.vote_average}
+                    </Card.Text>
+
+                </Card.Body>
             </Card>
         </div>
     )
--- pages/movies.tsx
+++ pages/movies.tsx
@@ -1,31 +1,53 @@
 import MovieTile from "../components/MovieTile";
+import {doc, getFirestore} from "firebase/firestore";
 
-export default function Home() {
+// export async function getServerSideProps(){
+//     const ref = doc(getFirestore(), `genres`);
+//     const queryGenres = query(
+//         ref,
+//         where('id','')
+//     )
+// }
+
+export default function Movies() {
+
+
+
     let movies = [
         {
             "poster_path": "/sDWARc5aYTUKE8Y2FIGVgWXuI4K.jpg",
             "title": "Twilight Zone: The Movie",
-            "vote_average":6.8
+            "release_date":"1998-08-28",
+            "vote_average":6.8,
+            "genres": [14]
         },
         {
             "poster_path": "/wkSzJs7oMf8MIr9CQVICsvRfwA7.jpg",
             "title": "Lost in Translation",
-            "vote_average":8.2
+            "release_date":"1998-08-28",
+            "vote_average":8.2,
+            "genres": [12]
         },
         {
             "poster_path": "/onmAHliMDEWTwksC7pAUpyVRZti.jpg",
             "title": "Barely Legal",
-            "vote_average":9.1
+            "release_date":"1990-08-28",
+            "vote_average":9.1,
+            "genres": [12,14]
         },
         {
             "poster_path": "/mTtgpH6UnHUtD8moRJUzfGLOZTj.jpg",
             "title": "The Last King of Scotland",
-            "vote_average":3.6
+            "release_date":"2003-08-28",
+            "vote_average":3.6,
+            "genres": [12,14,16]
         },
         {
             "poster_path": "/wkSzJs7oMf8MIr9CQVICsvRfwA7.jpg",
             "title": "Lost in Translation",
-            "vote_average":8.2
+            "release_date":"1999-08-28",
+            "vote_average":8.2,
+            "genres": [12,14]
         }
     ]
     return (
