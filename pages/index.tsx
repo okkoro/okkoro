1962ee8a45b6d0408dc97ecee4cd2aacaee2ad31
DuncanBH <2031593@champlaincollege.qc.ca>
2022-12-18 23:41:10 -0500

    Fetching movies from database (via 1 genre currently)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,34 @@
-export default function Home() {
+import {collection, getDocs, limit, orderBy, where} from "@firebase/firestore";
+import {getFirestore} from "firebase/firestore";
+import {query} from "@firebase/database";
+import {useState} from "react";
+import {docToJSON} from "../lib/firebase";
+
+const LIMIT = 10;
+
+export async function getServerSideProps() {
+    // TODO: Modify this query to suit our needs, currently only fetches comedy movies
+    const ref = collection(getFirestore(), 'movies');
+    const genreQuery = query(
+        // @ts-ignore
+        ref,
+        where('genre_ids', "array-contains", 35),
+        orderBy("id"),
+        limit(LIMIT)
+    )
+
+    // @ts-ignore
+    const movies = (await getDocs(genreQuery)).docs.map(docToJSON);
+
+    return {
+        props: {movies}
+    }
+
+}
+
+export default function Home(props: { movies: [any]; }) {
+    const [movies, setMovies] = useState(props.movies)
+
     return (
         <div className="row bg-green">
             <div className="col text-center">
