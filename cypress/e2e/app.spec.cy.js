e6557d4efdd3df475ef32d574d8c08a20bb75758
DuncanBH <2031593@champlaincollege.qc.ca>
2023-01-08 16:33:51 -0500

    Cypress testing for getting movies from database + eslint setup for Cypress

--- cypress/e2e/app.spec.cy.js
+++ cypress/e2e/app.spec.cy.js
@@ -1,7 +1,15 @@
 describe('Index Testing', () => {
-    it('should show a welcome heading', () => {
+    beforeEach(() => {
         cy.visit("/");
+    })
 
+    it('should show the homepage and movies from database', () => {
         cy.get('h1').contains('WELCOME TO OKKORO');
+
+        cy.get('h2').contains("Genre");
+
+        cy.get('[data-cy="Genre-MovieList"]').within(() => {
+            cy.get('[data-cy="MovieTile"]').should('be.visible');
+        })
     })
 })
