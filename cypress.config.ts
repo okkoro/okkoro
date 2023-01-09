908a3dceb24233f16abe76558c513a67a848c9a5
DuncanBH <2031593@champlaincollege.qc.ca>
2023-01-09 16:39:14 -0500

    Attempt at code coverage - disabled code coverage because missing instrumentation

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
     e2e: {
         setupNodeEvents(on, config) {
-            require('@cypress/code-coverage/task')(on, config)
-            return config
+            // require('@cypress/code-coverage/task')(on, config) //TODO: reenable once instrumentation is possible on SWC
+            // return config
 
             // implement node event listeners here
 
