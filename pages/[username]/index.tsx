e63827606fe0834d99d6cd84590a7cfc69c8e28b
Clément Cadieux <cadieuxclement@gmail.com>
2023-01-10 15:03:02 -0500

    Added button to account page

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,15 +1,33 @@
 import {useContext} from "react";
 import {UserContext} from "../../lib/context";
+import {Button, Col, Row} from "react-bootstrap";
+import {Console} from "inspector";
+
 
 export default function Page() {
     const { user, username } = useContext(UserContext);
 
+    const callApi = function (){
+        alert("Hello")
+    }
+    // @ts-ignore
+    // @ts-ignore
+    // @ts-ignore
     return (
-        <div className="row bg-green">
-            <div className="col text-center">
-                <img src="okkoro.png" />
-                <h1>Welcome {username}</h1>
-            </div>
+        <div className="bg-green">
+            <Row>
+                <Col className="text-center">
+                    <img src="okkoro.png" />
+                    <h1>Welcome {username}</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="flex-row-reverse">
+                    <div id="MovieResult"></div>
+                    {/*@ts-ignore*/}
+                    <Button onClick={() => callApi()}>Get Recommendations!</Button>
+                </Col>
+            </Row>
         </div>
     )
 }
