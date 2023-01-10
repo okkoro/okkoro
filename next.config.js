9cf7bdd9916719d1d1ad018c98af115e0e4b0206
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2023-01-09 22:34:48 -0500

    updated next_config so that google image domains are trusted

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ['image.tmdb.org']
+    domains: ['lh3.googleusercontent.com','image.tmdb.org'],
   }
 
 }
