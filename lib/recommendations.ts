85a7f1abb09242505a23dafaf1e03c4507a99798
Clément Cadieux <cadieuxclement@gmail.com>
2023-01-11 13:20:55 -0500

    Trying https

--- lib/recommendations.ts
+++ lib/recommendations.ts
@@ -3,6 +3,6 @@ import axios, {isCancel, AxiosError} from 'axios';
 
 
 export async function getRecommendation(){
-    return await axios.get("http://clementcadieux.pythonanywhere.com/api/recommendation");
+    return await axios.get("https://clementcadieux.pythonanywhere.com/api/recommendation");
 }
 
