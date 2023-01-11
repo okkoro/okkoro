ffe47db48964f81694386daa1de219883f4e2ac2
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2023-01-11 16:23:52 -0500

    added component test

---
+++ components/MovieTile.cy.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import MovieTile from "./MovieTile";
+
+
+let movie = {
+  id: 13,
+  poster_path: '/arw2vcBveWOVZr6pxd9XTd1TdQa.jpg',
+  original_language: 'en',
+  vote_average: 8.5,
+  genre_ids: [35, 18, 10749],
+  title: 'Forrest Gump',
+  overview: 'A man with a low IQ has accomplished great things in his life and been present during significant historic events—in each case, far exceeding what anyone imagined he could do. But despite all he has achieved, his one true love eludes him.',
+  release_date: '1994-06-23'
+};
+
+describe('<MovieTile />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<MovieTile  movie={movie} />)
+
+    cy.get(".card-title").contains(movie.title);
+  })
+})
