c3f29dcadfef49b0673ddb443ffd7b61381bb148
DuncanBH <2031593@champlaincollege.qc.ca>
2023-01-22 17:31:36 -0500

    Scrollbar design + MovieTile design rework

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,4 +1,4 @@
-import {Container} from "react-bootstrap";
+import {Row} from "react-bootstrap";
 
 import MovieTile from "./MovieTile";
 
--- components/MovieTile.tsx
+++ components/MovieTile.tsx
@@ -5,23 +5,23 @@ type propsType = {
     movie: Movie;
 }
 
-export default function MovieTile(props: propsType){
+export default function MovieTile(props: propsType) {
 
     return (
-        <div style={{margin:"1rem"}} data-cy={`MovieTile`}>
-            <Card className="bg-light-gray" style={{width:"14rem"}}>
-                <Link href={"/movies/" + props.movie.id}>
+        <div className={"m-2"} data-cy={`MovieTile`}>
+            <Card className="bg-light-gray cardAnim" style={{width: "14rem"}}>
+                <Link href={"/movies/" + props.movie.id} className={"text-decoration-none text-reset"}>
                     <CardImg src={"https://image.tmdb.org/t/p/w500" + props.movie.poster_path} alt="image of movie" />
-                </Link>
-                <Card.Body>
-                    <Card.Title data-cy={`MovieTitle`}>{props.movie.title}</Card.Title>
-                    <Card.Text className={"text-start"}>
-                        <b data-cy={`MovieReleaseDate`}>{props.movie.release_date.substring(0,4)}</b>
-                        <br/>
-                        {props.movie.vote_average}
-                    </Card.Text>
+                    <Card.Body className={"cardTextPos"}>
+                        <Card.Title data-cy={`MovieTitle`}>{props.movie.title}</Card.Title>
+                        <Card.Text className={"text-start  mb-auto"}>
+                            <b data-cy={`MovieReleaseDate`}>{props.movie.release_date.substring(0, 4)}</b>
+                            <br />
+                            {props.movie.vote_average}
+                        </Card.Text>
 
-                </Card.Body>
+                    </Card.Body>
+                </Link>
             </Card>
         </div>
     )
