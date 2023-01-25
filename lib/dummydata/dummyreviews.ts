fdc4e1362217d5e08a11669a33f67a7de2750b72
DuncanBH <2031593@champlaincollege.qc.ca>
2023-01-24 20:54:26 -0500

    Review type + dummy review data

---
+++ lib/dummydata/dummyreviews.ts
@@ -0,0 +1,28 @@
+const dummyreviews: Review[] = [
+    {
+        id: 0,
+        movieId: 100,
+        userId: "9pm6Ei3ezPMbdKv289MEapR4gaD3",
+        score: 4,
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tincidunt neque quis urna porta, ac pharetra nulla facilisis. Aenean malesuada lorem at orci efficitur molestie. Integer scelerisque massa et sagittis posuere. In in urna vel nulla vestibulum maximus nec et massa. Aliquam a varius arcu. Ut vel eros turpis. Nullam pulvinar in risus vitae ullamcorper. Cras cursus tempor mauris sed venenatis. Maecenas sodales leo eu consectetur aliquet. Aliquam ac auctor massa, id aliquam risus. \n Integer pharetra magna ac magna scelerisque, interdum luctus ante commodo. Nunc et vehicula nibh. Curabitur sagittis sapien nulla, tincidunt aliquam ante pretium scelerisque. Donec sed ex varius, tempor sem ut, laoreet nulla. Mauris ultricies, justo vitae pulvinar consectetur, diam nibh consectetur risus, nec congue ex augue id elit. Quisque sit amet porta augue, in imperdiet ligula. In pulvinar eget sapien ac feugiat. Fusce luctus, erat at placerat luctus, arcu augue ultrices ipsum, in feugiat mi leo ut mauris. Fusce eu quam semper, egestas nisl ut, fermentum lorem.",
+        date: "2007-06-23"
+    },
+    {
+        id: 1,
+        movieId: 100,
+        userId: "EvgPqp28ZAdECiUR5vUWepbTMZK2",
+        score: 2,
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tincidunt neque quis urna porta, ac pharetra nulla facilisis. Aenean malesuada lorem at orci efficitur molestie. Integer scelerisque massa et sagittis posuere. In in urna vel nulla vestibulum maximus nec et massa. Aliquam a varius arcu. Ut vel eros turpis. Nullam pulvinar in risus vitae ullamcorper. Cras cursus tempor mauris sed venenatis. Maecenas sodales leo eu consectetur aliquet. Aliquam ac auctor massa, id aliquam risus. \n Integer pharetra magna ac magna scelerisque, interdum luctus ante commodo. Nunc et vehicula nibh. Curabitur sagittis sapien nulla, tincidunt aliquam ante pretium scelerisque. Donec sed ex varius, tempor sem ut, laoreet nulla. Mauris ultricies, justo vitae pulvinar consectetur, diam nibh consectetur risus, nec congue ex augue id elit. Quisque sit amet porta augue, in imperdiet ligula. In pulvinar eget sapien ac feugiat. Fusce luctus, erat at placerat luctus, arcu augue ultrices ipsum, in feugiat mi leo ut mauris. Fusce eu quam semper, egestas nisl ut, fermentum lorem.",
+        date: "2003-07-15"
+    },
+    {
+        id: 2,
+        movieId: 100,
+        userId: "gbshXEk8WHWgvkzgp4fX4oRV9Aq2",
+        score: 3,
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tincidunt neque quis urna porta, ac pharetra nulla facilisis. Aenean malesuada lorem at orci efficitur molestie. Integer scelerisque massa et sagittis posuere. In in urna vel nulla vestibulum maximus nec et massa. Aliquam a varius arcu. Ut vel eros turpis. Nullam pulvinar in risus vitae ullamcorper. Cras cursus tempor mauris sed venenatis. Maecenas sodales leo eu consectetur aliquet. Aliquam ac auctor massa, id aliquam risus. \n Integer pharetra magna ac magna scelerisque, interdum luctus ante commodo. Nunc et vehicula nibh. Curabitur sagittis sapien nulla, tincidunt aliquam ante pretium scelerisque. Donec sed ex varius, tempor sem ut, laoreet nulla. Mauris ultricies, justo vitae pulvinar consectetur, diam nibh consectetur risus, nec congue ex augue id elit. Quisque sit amet porta augue, in imperdiet ligula. In pulvinar eget sapien ac feugiat. Fusce luctus, erat at placerat luctus, arcu augue ultrices ipsum, in feugiat mi leo ut mauris. Fusce eu quam semper, egestas nisl ut, fermentum lorem.",
+        date: "2005-01-04"
+    },
+]
+
+export default dummyreviews
