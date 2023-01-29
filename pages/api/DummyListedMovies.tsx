b4c0c82a85ddf020018a20b5e1e1f8b8bad25aee
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2023-01-29 15:49:00 -0500

    added dummy data to test user

--- pages/api/DummyListedMovies.tsx
+++ pages/api/DummyListedMovies.tsx
@@ -1,7 +1,7 @@
 import {setDoc} from "@firebase/firestore";
 import {doc, getFirestore} from "firebase/firestore";
 
-export default function handler(req, res) {
+export default function handler(req: { method: string; }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { test: string; }): void; new(): any; }; }; }) {
     const db = getFirestore()
     if (req.method === 'POST') {
         let listedMovies: ListedMovie[] = [{movieId:100,lists:["watched","liked"]},{movieId:10023,lists:["watched","liked"]},{movieId:10054,lists:["watched","liked"]},{movieId:10063,lists:["watched","liked"]},{movieId:10069,lists:["liked"]},{movieId:10072,lists:["watched"]}]
@@ -9,7 +9,7 @@ export default function handler(req, res) {
             listedMovies: listedMovies
         }
         setDoc(doc(db,"users", "qJac9C1xKyZvdNvdeLmty5rQsaX2"), data, { merge:true })
-            .then(docRef => {
+            .then(() => {
                 alert("Entire Document has been updated successfully");
             })
             .catch(error => {
