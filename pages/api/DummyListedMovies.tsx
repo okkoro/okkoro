475a9437a6188a9324384f60053df7ee4d9bbae0
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2023-01-29 15:47:53 -0500

    added dummy data to test user

--- pages/api/DummyListedMovies.tsx
+++ pages/api/DummyListedMovies.tsx
@@ -4,11 +4,11 @@ import {doc, getFirestore} from "firebase/firestore";
 export default function handler(req, res) {
     const db = getFirestore()
     if (req.method === 'POST') {
-        let listedMovies = [{movieId:100,lists:["watched","liked"]},{movieId:10023,lists:["watched","liked"]},{movieId:10054,lists:["watched","liked"]},{movieId:10063,lists:["watched","liked"]},{movieId:10069,lists:["liked"]},{movieId:10072,lists:["watched"]}]
+        let listedMovies: ListedMovie[] = [{movieId:100,lists:["watched","liked"]},{movieId:10023,lists:["watched","liked"]},{movieId:10054,lists:["watched","liked"]},{movieId:10063,lists:["watched","liked"]},{movieId:10069,lists:["liked"]},{movieId:10072,lists:["watched"]}]
         let data = {
             listedMovies: listedMovies
         }
-        setDoc(doc(db,"users", "gbshXEk8WHWgvkzgp4fX4oRV9Aq2"), data, { merge:true })
+        setDoc(doc(db,"users", "qJac9C1xKyZvdNvdeLmty5rQsaX2"), data, { merge:true })
             .then(docRef => {
                 alert("Entire Document has been updated successfully");
             })
