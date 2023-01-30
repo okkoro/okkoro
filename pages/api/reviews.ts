a23dcf8fb775782c94c3cb59011ec439dd530ca2
DuncanBH <2031593@champlaincollege.qc.ca>
2023-01-29 20:34:44 -0500

    Added PUT endpoint for editing reviews

--- pages/api/reviews.ts
+++ pages/api/reviews.ts
@@ -1,25 +1,49 @@
-import {addDoc, collection, serverTimestamp} from "@firebase/firestore";
-import {getFirestore} from "firebase/firestore";
+import {addDoc, collection, serverTimestamp, setDoc} from "@firebase/firestore";
+import {doc, getFirestore} from "firebase/firestore";
 
-export default function handler(req: any, res: any) {
-    const body = req.body;
+export default async function handler(req: any, res: any) {
+    if (req.method === 'POST') {
+        const body = req.body;
 
-    // console.log('body: ', body);
+        //Server side validation
+        if (!body.score || !body.text) {
+            return res.status(400).json({data: "Missing score or text"});
+        }
 
-    //Server side validation
-    if (!body.score || !body.text) {
-        return res.status(400).json({data: "Missing score or text"});
+        //Add timestamp to data
+        body.date = serverTimestamp();
+
+
+        addDoc(collection(getFirestore(), 'reviews'), body)
+            .catch((e) => {
+                console.log("failed adding new review: " + body);
+                throw e;
+            });
+
+        res.status(200).json({data: `Score: ${body.score} / Text: ${body.text}`});
+
+    } else if (req.method === 'PUT') {
+        const body = req.body;
+
+        const id = body.id;
+        delete body.id;
+
+        //Server side validation
+        if (!body.score || !body.text) {
+            return res.status(400).json({data: "Missing score or text"});
+        }
+        //TODO: possibly add security checks around ID and username?
+
+        //Add timestamp to data
+        body.date = serverTimestamp();
+
+        setDoc(doc(getFirestore(), 'reviews', id), body)
+            .catch((e) => {
+                console.log("failed adding new review: " + body);
+                throw e;
+            });
+
+        res.status(200).json({data: `Score: ${body.score} / Text: ${body.text}`});
+
     }
-
-    //Add timestamp to data
-    body.date = serverTimestamp();
-
-
-    addDoc(collection(getFirestore(), 'reviews'), body)
-        .catch((e) => {
-            console.log("failed adding new review: " + body);
-            throw e;
-        });
-
-    res.status(200).json({data: `Score: ${body.score} / Text: ${body.text}`});
 }
