0cd4509fe705b3d421fc89d32e7bac89d26a4c41
DuncanBH <2031593@champlaincollege.qc.ca>
2023-02-10 15:16:21 -0500

    Reworked api endpoint into local function

--- lib/reviews.ts
+++ lib/reviews.ts
@@ -1,6 +1,8 @@
 import {addDoc, collection, serverTimestamp, setDoc} from "@firebase/firestore";
 import {doc, getFirestore} from "firebase/firestore";
 
+//formerly an endpoint, cannot get firebase to connect on serverless functions so moving it here.
+/*
 export default async function handler(req: any, res: any) {
     if (req.method === 'POST') {
         const body = req.body;
@@ -45,5 +47,47 @@ export default async function handler(req: any, res: any) {
 
         res.status(200).json({data: `Score: ${body.score} / Text: ${body.text}`});
 
+    }
+}
+*/
+
+async function createReview(review: Review) {
+    //validation
+    if (!review.score || !review.text) {
+        return null;
+    }
+
+    //Add timestamp to data
+    review.date = serverTimestamp();
+
+    addDoc(collection(getFirestore(), 'reviews'), review)
+        .catch((e) => {
+            console.log("failed adding new review: " + review);
+            throw e;
+        });
+
+    return review;
+}
+
+async function updateReview(review: Review) {
+    if (review.id) {
+        const id = review.id.toString();
+        delete review.id;
+
+        if (!review.score || !review.text) {
+            return null;
+        }
+
+        review.date = serverTimestamp();
+
+        setDoc(doc(getFirestore(), 'reviews', id), review)
+            .catch((e) => {
+                console.log("failed adding new review: " + review);
+                throw e;
+            });
+
+        return review;
+    } else {
+        return null;
     }
 }
