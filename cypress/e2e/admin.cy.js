02c564bb82652aab6426071d18d0113adb724b1c
DuncanBH <2031593@champlaincollege.qc.ca>
2023-02-11 13:47:46 -0500

    Added admin testing file

---
+++ cypress/e2e/admin.cy.js
@@ -0,0 +1,24 @@
+describe('Admin Testing', () => {
+    beforeEach(() => {
+        cy.visit("/");
+    })
+
+    it('should not have admin indicator in navbar', () => {
+        cy.get('[data-cy="Admin-NavIndicator"]').should('not.exist');
+    })
+
+    it('should not find the review admin delete button', () => {
+        //Navigate to movie page
+        cy.get('[data-cy="Movies-NavButton"]').click();
+        cy.get('[data-cy="Dummy placeholder movies to be filled in-MovieList"]').within(() => {
+            cy.get('[data-cy="MovieTile"]').should('be.visible');
+            cy.get('[data-cy="MovieTile"]').first().click();
+        })
+        cy.get('[data-cy="MovieTitle"]')
+
+        //Get to dropdown menu
+        cy.get('[data-cy="Reviews-Dropdown"]').first().click();
+        cy.get('[data-cy="Reviews-Dropdown-AdminDelete"]').should('not.exist');
+    })
+
+})
