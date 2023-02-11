75c3ad674ffa6b020b3e2c5e6d23e743bd81efe0
DuncanBH <2031593@champlaincollege.qc.ca>
2023-02-11 17:01:05 -0500

    Fixed styling on add to list Page

--- pages/movies/[movieId]/lists.tsx
+++ pages/movies/[movieId]/lists.tsx
@@ -2,7 +2,7 @@ import {useContext, useState} from "react";
 import {UserContext} from "../../../lib/context";
 import {useRouter} from "next/router";
 import {getMovieById, getUserByUsername, addMovieToList} from "../../../lib/firebase";
-import {Col, Container, Image, Row} from "react-bootstrap";
+import {Col, Container, Form, Image, Row} from "react-bootstrap";
 
 export default function AddToList() {
     // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -57,23 +57,26 @@ export default function AddToList() {
                 {movieDetails != null && (
                     <Container>
                         <Row>
-                            <Col>
-                                <Image className={"w-75 p-3"} style={{borderRadius: 25}} src={"https://image.tmdb.org/t/p/w500" + movieDetails.poster_path} alt="image of movie" />
-                            </Col>
-                            <Col className={"col-lg-5 col-md-6"}>
-                                <h1 data-cy={`MovieTitle`}>{movieDetails.title}</h1>
-                                <p>{movieDetails.release_date}</p>
-                                <p>{movieDetails.vote_average}/10</p>
-                                <p>{movieGenres}</p>
-                            </Col>
-                            <Col className={"col-lg-2 col-md-6"}>
-                                <form action={"/movies/" + movieId}>
-                                    <select data-cy={"list-selector"} id={"list-selector"}>
-                                        {lists.map((list) => <option key={list} value={list}>{list.charAt(0).toUpperCase() + list.substring(1)}</option>)}
-                                    </select>
+                            <Col className={"col-6 d-flex flex-row"}>
+                                <Image className={"w-50 p-3"} style={{borderRadius: 25}} src={"https://image.tmdb.org/t/p/w500" + movieDetails.poster_path} alt="image of movie" />
 
-                                    <input type={"submit"} value={"Add to this list"} onClick={addToList} data-cy={"submit-button"}></input>
-                                </form>
+                                <div className={"p-2"}>
+                                    <h1 data-cy={`MovieTitle`}>{movieDetails.title}</h1>
+                                    <p>{movieDetails.release_date}</p>
+                                    <p>{movieDetails.vote_average}/10</p>
+                                    <p>{movieGenres}</p>
+                                </div>
+                            </Col>
+                            <Col className={"col-4 p-3 text-center"}>
+                                <Form action={"/movies/" + movieId}>
+                                    <Form.Label><h4>Add {movieDetails.title} to list:</h4></Form.Label>
+                                    
+                                    <Form.Select data-cy={"list-selector"} id={"list-selector"}>
+                                        {lists.map((list) => <option key={list} value={list}>{list.charAt(0).toUpperCase() + list.substring(1)}</option>)}
+                                    </Form.Select>
+
+                                    <Form.Control type={"submit"} className={"btn btn-green w-50 mt-3"} value={"Save"} onClick={addToList} data-cy={"submit-button"}/>
+                                </Form>
                             </Col>
                         </Row>
                     </Container>
