be8635851d7d5e9e3aafc5f2e54d4ea48ac0d652
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2023-02-12 19:13:09 -0500

    added report type

---
+++ lib/types/Report.d.ts
@@ -0,0 +1,16 @@
+type Report = {
+    id?: number;
+    date: any;
+
+    reason: string;
+
+    removed: boolean;
+
+    reviewId: string;
+
+    reviewed: boolean;
+
+    text: string;
+
+    userId: string;
+};
