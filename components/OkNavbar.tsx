8844c969c538f728d00695fcfd80a79ab2e2df13
DuncanBH <2031593@champlaincollege.qc.ca>
2023-02-14 22:30:58 -0500

    Initial I18N set up + demo

--- components/OkNavbar.tsx
+++ components/OkNavbar.tsx
@@ -4,9 +4,12 @@ import {Button, Nav, Navbar, NavbarBrand} from "react-bootstrap";
 import Link from "next/link";
 import logo from "public/okkoro_banner.png";
 import Image from "next/image";
+import {useTranslation} from "react-i18next";
 
 export default function OkNavbar() {
     const {user, username, admin} = useContext(UserContext);
+
+    const { t } = useTranslation();
 
     return (
         <Navbar className={"border-bottom border-dark-gray sticky-top bg-black pe-1"}>
@@ -18,7 +21,7 @@ export default function OkNavbar() {
 
             <Nav className={"me-auto"}>
                 <Link data-cy={"Movies-NavButton"} href={`/movies`} className={'text-decoration-none px-3 text-green border-start border-green'} style={{fontSize: "1.5rem"}}>
-                    Movies
+                    {t("navbarMovies")}
                 </Link>
             </Nav>
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,14 +7,29 @@ import {Container, SSRProvider} from "react-bootstrap";
 import '../styles/main.scss';
 import {Toaster} from "react-hot-toast";
 
+import './../lib/i18n'
+import {useTranslation} from "react-i18next";
+
+const lngs: any = {
+    "en": { nativeName: "English"},
+    "fr": {nativeName: "Français"}
+}
 export default function App({Component, pageProps}: AppProps) {
     const userData = useUserData();
+    const {t, i18n} = useTranslation();
 
     return (
         <SSRProvider>
             {/* @ts-ignore */}
             <UserContext.Provider value={userData}>
                 <Container fluid className={"bg-black text-light-gray main"} style={{minHeight: "100vh"}}>
+                    <div>
+                        {Object.keys(lngs).map((lng) => (
+                            <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
+                                {lngs[lng].nativeName}
+                            </button>
+                        ))}
+                    </div>
                     <OkNavbar />
                     <Component {...pageProps} />
                     <Toaster />
